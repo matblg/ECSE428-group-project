5a051b930eb88a24098560ef142acd6179322393
Marianne Romero <marianne.romero@mail.mcgill.ca>
2025-10-18 13:29:20 -0400

    TID003

---
+++ backend/src/test/java/ca/mcgill/ecse428/letterbook/CucumberTest.java
@@ -0,0 +1,8 @@
+package ca.mcgill.ecse428.letterbook;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+public class CucumberTest {
+
+}
---
+++ backend/src/test/java/ca/mcgill/ecse428/letterbook/StepDefenitionsUser.java
@@ -0,0 +1,5 @@
+package ca.mcgill.ecse428.letterbook;
+
+public class StepDefenitionsUser {
+    
+}
