bf6ef90590313b1dcbfea19278f8b18ff43e10a6
Najib Najib <najibnajib@MacBook-Pro-de-Najib.local>
2025-10-23 21:10:44 -0400

    added more elements and fixes to the dto

--- backend/src/main/java/ca/mcgill/ecse428/letterbook/dto/BookItemDTO.java
+++ backend/src/main/java/ca/mcgill/ecse428/letterbook/dto/BookItemDTO.java
@@ -1,10 +1,25 @@
 package ca.mcgill.ecse428.letterbook.dto;
 
+import java.util.List;
+
 public class BookItemDTO {
     
     public String kind;
     public String id;
     public String etag;
     public String selfLink;
-    public 
+
+    public String title;
+    public List<String> authors;
+    public String publishedDate;
+    public String thumbnail;
+    public String description;
+    public String infoLink;
+    public String previewLink;
+    public String isbn13;
+    public Integer pageCount;
+    public List<String> categories;
+    public String language;
+    public Double averageRating;
+    public Integer ratingsCount;
 }
--- backend/src/main/java/ca/mcgill/ecse428/letterbook/dto/BookSearchResultDTO.java
+++ backend/src/main/java/ca/mcgill/ecse428/letterbook/dto/BookSearchResultDTO.java
@@ -2,10 +2,61 @@ package ca.mcgill.ecse428.letterbook.dto;
 
 import java.util.List;
 
-
-
 public class BookSearchResultDTO {
     private String kind;
     private int totalItems;
     private List<BookItemDTO> items;
+    private Integer nextStartIndex;
+    private Integer pageSize;
+
+    public BookSearchResultDTO() {
+    }
+
+    public BookSearchResultDTO(String kind, int totalItems, List<BookItemDTO> items, Integer nextStartIndex, Integer pageSize) {
+        this.kind = kind;
+        this.totalItems = totalItems;
+        this.items = items;
+        this.nextStartIndex = nextStartIndex;
+        this.pageSize = pageSize;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public int getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(int totalItems) {
+        this.totalItems = totalItems;
+    }
+
+    public List<BookItemDTO> getItems() {
+        return items;
+    }
+
+    public void setItems(List<BookItemDTO> items) {
+        this.items = items;
+    }
+
+    public Integer getNextStartIndex() {
+        return nextStartIndex;
+    }
+
+    public void setNextStartIndex(Integer nextStartIndex) {
+        this.nextStartIndex = nextStartIndex;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
 }
