4ff2470aebf4c1f5361a2de8d7eca21d1d989dda
ReinaEl-Hoz <rehz2004@gmail.com>
2025-10-24 19:54:11 -0400

    changed to MongoRepo

--- backend/src/main/java/ca/mcgill/ecse428/letterbook/repository/UserRepository.java
+++ backend/src/main/java/ca/mcgill/ecse428/letterbook/repository/UserRepository.java
@@ -1,14 +1,12 @@
 package ca.mcgill.ecse428.letterbook.repository;
 
-import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import ca.mcgill.ecse428.letterbook.model.User;
 
-public interface UserRepository extends CrudRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, String> {
     public Optional<User> findByUsername(String username);
     public Optional<User> findByEmail(String email);
-    public List<User> findAll();
 }
