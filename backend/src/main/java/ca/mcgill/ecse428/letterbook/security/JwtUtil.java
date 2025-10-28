d40340205d51dc3c5949d2ef41f3c02a38301452
Mathis Bélanger <mathisbelangerr@gmail.com>
2025-10-27 22:24:22 -0400

    set secret, should use env, but wtv

--- backend/src/main/java/ca/mcgill/ecse428/letterbook/security/JwtUtil.java
+++ backend/src/main/java/ca/mcgill/ecse428/letterbook/security/JwtUtil.java
@@ -4,6 +4,8 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -12,10 +14,11 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private static final String SECRET = "ChangeThisSecretToEnvVarChangeThisSecretToEnvVar"; // >= 256 bits for HS256
+    @Value("${jwt.secret}")
+    private String secret;
 
     private Key getSigningKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
